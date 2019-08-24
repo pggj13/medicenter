a182613631f31752ce7cd842c8b26e250666265a
pggj13 <paulogoncalo@outlook.com>
2019-08-23 21:29:24 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,310 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"content="width=device-width,user-scalable-0"/>
+        <title>MediCenter</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="logo">
+                    <a href=""><img src="assets/images/logo.png"/></a>
+                </div>
+                <div class="menu">
+                    <nav>
+                        <ul>
+                            <li class="active"><a href="">HOME</a></li>
+                            <li><a href="">HOME</a></li>
+                            <li><a href="">HOME</a></li>
+                            <li><a href="">HOME</a></li>
+                            <li><a href="">HOME</a></li>
+                            <li><a href="">HOME</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <section id="banner">
+            <div class="container column">
+                <div class="banner_headline">
+                    <h1>Top notch Experience</h1>
+                    <h2>Medicenter is a responsive template perfect for all screen sizes</h2>
+                </div>
+                <div class="banner_options">
+                    <div class="banner1">
+                        <div class="banner_title">Emergency Case</div>
+                        <div class="banner_desc">If you needs a doctor urgenty outside of medicenter 
+                            opening hours.call emergency appoinment number for emergency service.
+                        </div>
+                        <a href="">Read more</a>
+                    </div>
+                    <div class="banner2">
+                        <div class="banner_title">Doctors Timetable</div>
+                        <div class="banner_desc">Here at medicenter we have individual doctors
+                            lives. Click read more bellow to see services and current timetable for our doctors.
+                        </div>
+                        <a href="">Read more</a>
+                    </div>
+                    <div class="banner3">
+                        <div class="banner_title">Opening Hours</div>
+                        <div class="banner_desc_hours">
+                            <div class="banner_desc_day">Monday - Friday</div>
+                            <div class="banner_desc_hour">8:00 - 17:00</div>
+                        </div>
+                        <div class="banner_desc_hours">
+                            <div class="banner_desc_day">Sartuday</div>
+                            <div class="banner_desc_hour">9:30 - 17:30</div>
+                        </div>
+                        <div class="banner_desc_hours">
+                            <div class="banner_desc_day">Sunday</div>
+                            <div class="banner_desc_hour">9:30 - 15:00</div>
+                        </div>
+                        <a href="">Read more </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="geral">
+            <div class="container">
+                <section>
+                    <div class="widget">
+                        <div class="widget_title">
+                            <div class="widget_title_text">Latest News</div>
+                            <div class="wiget_title_bar"></div>
+                        </div>
+                        <div class="widget_body flex">
+                            <article>
+                                <a href="">
+                                    <div class="news_data">
+                                        <div class="news_posted_at">12 DEC 12</div>
+                                        <div class="news_posted_comment">2</div>
+                                    </div>
+                                    <div class="news_thumbnail">
+                                        <img src="assets/images/dr.jpg"/>
+                                    </div>
+                                    <div class="news_title">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria
+                                    </div>
+                                    <div class="news_resume">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI, 
+                                        quando um impressor desconhecido pegou uma bandeja de tipos e os 
+                                        embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum 
+                                        sobreviveu não só a cinco séculos, como também ao salto para a editoração
+                                        eletrônica, permanecendo essencialmente inalterado. Se popularizou na 
+                                        década de 60, quando a Letraset lançou decalques contendo passagens de 
+                                        Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares 
+                                        de editoração eletrônica como Aldus PageMaker.
+                                    </div>
+                                </a>
+                            </article>
+                            <article>
+                                <a href="">
+                                    <div class="news_data">
+                                        <div class="news_posted_at">12 DEC 12</div>
+                                        <div class="news_posted_comment">2</div>
+                                    </div>
+                                    <div class="news_thumbnail">
+                                        <img src="assets/images/dr.jpg"/>
+                                    </div>
+                                    <div class="news_title">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria
+                                    </div>
+                                    <div class="news_resume">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI, 
+                                        quando um impressor desconhecido pegou uma bandeja de tipos e os 
+                                        embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum 
+                                        sobreviveu não só a cinco séculos, como também ao salto para a editoração
+                                        eletrônica, permanecendo essencialmente inalterado. Se popularizou na 
+                                        década de 60, quando a Letraset lançou decalques contendo passagens de 
+                                        Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares 
+                                        de editoração eletrônica como Aldus PageMaker.
+                                        sobreviveu não só a cinco séculos, como também ao salto para a editoração
+                                        eletrônica, permanecendo essencialmente inalterado. Se popularizou na 
+                                        década de 60, quando a Letraset lançou decalques contendo passagens de 
+                                        Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares 
+                                        de editoração eletrônica como Aldus PageMaker.
+                                    </div>
+                                </a>
+                            </article>
+                            <article>
+                                <a href="">
+                                    <div class="news_data">
+                                        <div class="news_posted_at">12 DEC 12</div>
+                                        <div class="news_posted_comment">2</div>
+                                    </div>
+                                    <div class="news_thumbnail">
+                                        <img src="assets/images/dr.jpg"/>
+                                    </div>
+                                    <div class="news_title">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria
+                                    </div>
+                                    <div class="news_resume">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI, 
+                                        quando um impressor desconhecido pegou uma bandeja de tipos e os 
+                                        embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum 
+                                        sobreviveu não só a cinco séculos, como também ao salto para a editoração
+                                        eletrônica, permanecendo essencialmente inalterado. Se popularizou na 
+                                        década de 60, quando a Letraset lançou decalques contendo passagens de 
+                                        Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares 
+                                        de editoração eletrônica como Aldus PageMaker.
+                                    </div>
+                                </a>
+                            </article>
+                            <article>
+                                <a href="">
+                                    <div class="news_data">
+                                        <div class="news_posted_at">12 DEC 12</div>
+                                        <div class="news_posted_comment">2</div>
+                                    </div>
+                                    <div class="news_thumbnail">
+                                        <img src="assets/images/dr.jpg"/>
+                                    </div>
+                                    <div class="news_title">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria
+                                    </div>
+                                    <div class="news_resume">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI, 
+                                        quando um impressor desconhecido pegou uma bandeja de tipos e os 
+                                        embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum 
+                                        sobreviveu não só a cinco séculos, como também ao salto para a editoração
+                                        eletrônica, permanecendo essencialmente inalterado. Se popularizou na 
+                                        década de 60, quando a Letraset lançou decalques contendo passagens de 
+                                        Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares 
+                                        de editoração eletrônica como Aldus PageMaker.
+                                    </div>
+                                </a>
+                            </article>
+                        </div>
+                    </div>
+                </section>
+                <aside>
+                    <div class="widget">
+                        <div class="widget_title">
+                            <div class="widget_title_text">Deparments</div>
+                            <div class="wiget_title_bar"></div>
+                        </div>
+                        <div class="widget_body">
+                            ....
+                        </div>
+                    </div>
+                    <div class="widget">
+                        <div class="widget_title">
+                            <div class="widget_title_text">Make An Appointment</div>
+                            <div class="wiget_title_bar"></div>
+                        </div>
+                        <div class="widget_body">
+                            ....
+                        </div>
+                    </div>
+                </aside>
+            </div>
+        </section>
+        <footer id="footer">
+            <div class="container footer_column">
+                <div class="footer_options">
+                    <div class="footer1">
+                        <div class="footer_title">Health Insacure</div>
+                        <div class="footer_desc">If you needs a doctor urgenty outside
+                        </div>
+                    </div>
+                    <div class="footer2">
+                        <div class="footer_title">Medical Record</div>
+                        <div class="footer_desc">If you needs a doctor urgenty outside
+                        </div>
+                    </div>
+                    <div class="footer3">
+                        <div class="footer_title">Online Bill</div>
+                        <div class="footer_desc">If you needs a doctor urgenty outside
+                        </div>
+                    </div>
+                </div>
+                <div class="footerarea">
+                    <div class="footerarea1">
+                        <div class="widget">
+                            <div class="widget_title">
+                                <div class="widget_title_text">Medicenter Clinic</div>
+                                <div class="wiget_title_bar"></div>
+                            </div>
+                            <div class="widget_body">
+                                <div class="widget_body footer_body">
+                                    <div class="footer_body_item">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+                                    </div>
+                                    <div class="footer_body_item">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+                                    </div>
+                                    <div class="footer_body_item">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footerarea2">
+                        <div class="widget">
+                            <div class="widget_title">
+                                <div class="widget_title_text">Lasted Posted</div>
+                                <div class="wiget_title_bar"></div>
+                            </div>
+                            <div class="widget_body footer_body">
+                                <div class="footer_body_item footer_post">
+                                    Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                    tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+                                </div>
+                                <div class="day"> 9 years ago</div>
+                                <div class="footer_body_item footer_post">
+                                    Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                    tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+                                </div>
+                                <div class="day"> 9 years ago</div>
+                                <div class="footer_body_item footer_post">
+                                    Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                    tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+                                </div>
+                                <div class="day"> 9 years ago</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footerarea3">
+                        <div class="widget">
+                            <div class="widget_title">
+                                <div class="widget_title_text">Lasted Twitter</div>
+                                <div class="wiget_title_bar"></div>
+                            </div>
+                            <div class="widget_body">
+                                <div class="widget_body footer_body">
+                                    <div class="footer_body_item">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+                                    </div>
+                                    <div class="day"> 9 years ago</div>
+                                    <div class="footer_body_item">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+                                    </div>
+                                    <div class="day"> 9 years ago</div>
+                                    <div class="footer_body_item">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+                                    </div>
+                                    <div class="day"> 9 years ago</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer_copy">
+                    Copyright - Todos os direitos reservados
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
